6e61c5307a9ddf1b6b05aa4782b05f981e84e4c3
ruiqi <ruiqi@appledeMacBook-Air-2.local>
2019-03-03 16:47:28 -0500

    reorganize the router

--- index.js
+++ index.js
@@ -1,44 +1,56 @@
 var express = require('express');
 var app = express();
-var router = express.Router();
 var path = require('path');
 var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb://130.245.170.98:27017/";
 
+// get form data from req body
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 
-//app.get('/', function(req, res){
-router.get('/', function(req, res){
-    res.sendFile(path.join(__dirname+'/index.html'));
+// routers
+var login = require('./routes/login.js');
+var adduser = require('./routes/adduser.js');
+var verify = require('./routes/verify.js');
+
+// add api
+app.use('/login', login);
+app.use('/adduser', adduser);
+app.use('/verify', verify);
+
+app.get('/', function(req, res){
+    res.sendFile(path.join(__dirname+'/views/index.html'));
 });
 
-app.post('/login', function(req, res){
-    var name = req.body.name;
-    res.send('Hi, '+name);
-    MongoClient.connect(url, function(err, db) {
-      if (err) throw err;
-      var dbo = db.db("warmup");
-      var myobj = { name: req.body.name, email: req.body.name, psword: req.body.psword };
-      dbo.collection("users").insertOne(myobj, function(err, res) {
-        if (err) throw err;
-        console.log("1 document inserted");
-        db.close();
-      });
-    });
-
+//app.post('/login', function(req, res){
+//    var name = req.body.name;
+//    res.send('Hi, '+name);
+MongoClient.connect(url, function(err, db) {
+    if(err){
+      console.log(err);
+    }else{
+      console.log("Success connect to database");
+    }
+    var dbo = db.db("warmup");
+    app.locals.db = dbo;
+    //var myobj = { name: req.body.name, email: req.body.name, psword: req.body.psword };
+    //dbo.collection("users").insertOne(myobj, function(err, res) {
+    //    if (err) throw err;
+    //    console.log("1 document inserted");
+    //    db.close();
+    //  });
 });
+//});
 
 router.get('/ttt/', function(req, res){
-     res.sendFile(path.join(__dirname+'/views/ttt/in.html'));
+    res.sendFile(path.join(__dirname+'/views/ttt/in.html'));
 });
 
 
 
-app.use('/', router);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
---
+++ routes/adduser.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+// get form data from req body
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// create application/json parser
+var jsonParser = bodyParser.json();
+
+// GET users listing.
+router.get('/', function(req, res, next) {
+  res.sendFile(path.join(__dirname+'/views/adduser.html'));
+});
+
+router.post('/', jsonParser, function(req, res) {
+  json = {status:"OK"};
+  data = req.body;
+
+  var db = req.app.locals.db;
+  
+  // add the new user to mongodb
+  db.collection("users").insertOne(data, function(err, res){
+    if (err) {
+      console.log(err);
+      json.status="ERROR";
+  }else{
+      console.log("1 document inserted");
+      sendMail(data);
+      console.log(data);
+  }
+  res.json({ status:'OK'});
+  });
+});
+
+module.exports = router;
