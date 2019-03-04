53b183c5ff224e0a3e6298496206bac4a85f2a5a
ruiqi <ruiqi@appledeMacBook-Air-2.local>
2019-03-03 22:44:42 -0500

    test email send

--- routes/adduser.js
+++ routes/adduser.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var path = require('path');
+const nodemailer = require('nodemailer');
 
 // get form data from req body
 var bodyParser = require("body-parser");
@@ -19,6 +20,7 @@ router.post('/', jsonParser, function(req, res) {
   data = req.body;
   data['valid'] = "false";
   data['key'] = Math.floor((Math.random() * 8999) + 1000);
+  data['current_grid'] = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
 
   var db = req.app.locals.db;
   
@@ -29,11 +31,40 @@ router.post('/', jsonParser, function(req, res) {
       json.status="ERROR";
   }else{
       console.log("1 document inserted");
-      //sendMail(data);
+      sendMail(data);
       console.log(data);
   }
   res.json({status:'OK'});
   });
 });
+
+function sendMail(data){
+  //console.log("data:" ,data);
+  var transporter = nodemailer.createTransport({
+      //host: 'email.cloud.compas.cs.stonybrook.edu',
+      host:'smtp.gmail.com',
+      port:465,
+      secure:true,
+      auth: {
+          user: 'cse356test@gmail.com',
+          pass: 'Cse356lalala'
+      }
+  });
+  var mailOpton = {
+      //from:'cse356@email.cloud.compas.cs.stonybrook.edu',
+      from: 'cse356test@gmail.com',
+      to: data.email,
+      subject: "verify code",
+      text: "key"+data.key
+  };
+
+  transporter.sendMail(mailOpton, function(error, info){
+      if (error) {
+        console.log("error is:");
+        console.log(error);
+      } 
+        else{console.log('Email sent: ')}
+      });
+}
 
 module.exports = router;
